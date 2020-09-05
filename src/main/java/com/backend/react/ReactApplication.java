42c7d858203674b778c6a6a3f8cb4c2657bf3514
mustafaaksu <mustafaaksu9079@gmail.com>
2020-09-05 22:17:08 +0300

    Initial commit

---
+++ src/main/java/com/backend/react/ReactApplication.java
@@ -0,0 +1,13 @@
+package com.backend.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactApplication.class, args);
+	}
+
+}
